d0ea5a2318cce6bfc2fe7752a06aa771f2b57a54
flvSantos15 <flvSantos300@gmail.com>
2021-08-18 22:26:35 -0300

    component home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,23 @@
 import Head from 'next/head'
 
+import styles from './home.module.scss'
+
 export default function Home() {
   return (
     <>
       <Head>
-          <title>Início | ig.news</title>
-        </Head>
-      
+        <title>Home | ig.news</title>
+      </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span> 👏 Hey, welcome</span>
+          <h1>New about the <span>React</span> world</h1>
+          <p>Get acess to all the publications<br/>
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl Coding" />
+      </main>
     </>
     )
 }
