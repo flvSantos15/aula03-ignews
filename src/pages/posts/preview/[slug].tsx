e1438e6917ae1470ca85e5f4afc8d49928a8f7dc
flvSantos15 <48564704+flvSantos15@users.noreply.github.com>
2021-11-23 23:34:59 -0300

    gerando previews estaticos

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -1,4 +1,4 @@
-import { GetStaticProps } from "next"
+import { GetStaticPaths, GetStaticProps } from "next"
 import Link from 'next/link'
 import Head from 'next/head'
 import { RichText } from "prismic-dom"
@@ -54,7 +54,7 @@ export default function PostPreview({ post }: PostPreviewProps) {
   )
 }
 
-export const getStaticPaths = () => {
+export const getStaticPaths: GetStaticPaths = async () => {
   return {
     paths: [],
     fallback: 'blocking'
@@ -81,6 +81,7 @@ export const getStaticProps: GetStaticProps = async ({params}) => {
   return {
     props: {
       post,
-    }
+    },
+    redirect: 60 * 30, // 30 minutos
   }
 }
